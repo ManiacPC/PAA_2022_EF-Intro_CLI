84768f7d380a6fc154981b1a48fcc0e13e49f1eb
Enrique Palacios <epalaciosc@pixisproti.cl>
2022-07-18 15:02:35 -0400

    Add project files.

---
+++ PAA_2022_EF-Intro/Models/Album.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PAA_2022_EF_Intro.Models
+{
+    [Table("Album")] // Indicar la tabla al que pertenecerá la clase POCO
+    public class Album
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public DateTime Lanzamiento { get; set; }
+        public bool? TopSeller { get; set; }
+        public string Productora { get; set; }
+
+        // Relaciones
+        public virtual ICollection<Cancion> Canciones { get; set; }
+    }
+}
---
+++ PAA_2022_EF-Intro/Models/Cancion.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PAA_2022_EF_Intro.Models
+{
+    [Table("Cancion")] // Indicar la tabla al que pertenecerá la clase POCO
+    public class Cancion
+    {
+        public int Id { get; set; }
+        public int AlbumId { get; set; }
+        public string Titulo { get; set; }
+        public int Duracion { get; set; }
+
+        // Relaciones
+        public virtual Album Album { get; set; }
+    }
+}
